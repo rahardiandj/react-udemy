de474fb714615d208838ecf629ca41871317d50b
Rahardian Juliarto <rahardian.juliarto@traveloka.com>
2019-10-28 00:22:57 +0700

    Add blog project for more advanced react-redux

---
+++ blog/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PostList from './PostList'
+
+const App = () => {
+    return (
+        <div className="ui component"><PostList /></div>
+    );
+};
+
+export default App;
