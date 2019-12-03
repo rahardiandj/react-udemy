63e3638da7b630d0e8fbd4e85b48b7ba48ea81bf
Rahardian Juliarto <rahardian.juliarto@traveloka.com>
2019-12-04 06:46:30 +0700

    Add color context

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component {
     state = { language :'english' };
@@ -18,9 +19,11 @@ class App extends React.Component {
                 <i className="flag us" onClick={() => this.onLanguageChange('english')} />
                 <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
             </div>
-            <LanguageContext.Provider value={this.state.language}>
-                <UserCreate />
-            </LanguageContext.Provider>
+            <ColorContext.Provider value="red" >
+                <LanguageContext.Provider value={this.state.language}>
+                    <UserCreate />
+                </LanguageContext.Provider>
+            </ColorContext.Provider>
         </div>)
     }
 }
--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
+
+    renderButton (color) {
+        return (
+            <button className={`ui button ${color}`}>
+                <LanguageContext.Consumer>
+                    {(value) => value ==='english' ? 'Submit' : 'Voorlegen'}
+                </LanguageContext.Consumer>
+            </button>
+        )
+    }
 
 
     render() {
         return(
-        <button className="ui button primary">
-        <LanguageContext.Consumer>
-            {(value) => value ==='english' ? 'Submit' : 'Voorlegen'}
-        </LanguageContext.Consumer>
-        </button>
+        <ColorContext.Consumer>
+            {(color) => this.renderButton(color)}
+        </ColorContext.Consumer>
         );
     }
 }
